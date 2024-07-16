f2384cd4dcfc2e6a43facd919e3b21e196a768e0
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-07-17 06:38:39 +0800

    Restructure and rename

--- Function.py
+++ Function.py
@@ -1,26 +1,25 @@
-from numpy import *
-from matplotlib.pyplot import *
+import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
 def _getAera(lstVertice):
     # Ensure the vertices are a NumPy array for efficient computation
-    lstVertice = asarray(lstVertice)
+    lstVertice = np.asarray(lstVertice)
     
     # Extract x and y coordinates
     x = lstVertice[:,0]
     y = lstVertice[:,1]
     
     # Compute the area using the Shoelace formula
-    area = 0.5*abs(dot(x, roll(y,1)) - dot(y, roll(x,1)))
+    area = 0.5*np.abs(np.dot(x, np.roll(y,1)) -np. dot(y, np.roll(x,1)))
     return area
 
-def getDs(traj:ndarray) -> tuple[ndarray, Voronoi]:
+def getDs(traj:np.ndarray) -> tuple[np.ndarray, Voronoi]:
     numPt = traj.shape[0]
     objVor = Voronoi(traj)
     lstVert = objVor.vertices
     lstRegion = [objVor.regions[i] for i in objVor.point_region]
     lstCntRep = [sum(objVor.point_region == objVor.point_region[i]) for i in range(numPt)]
-    lstDs = zeros([numPt], dtype=float64)
+    lstDs = np.zeros([numPt], dtype=np.float64)
     for idxDs in range(numPt):
         if -1 in lstRegion[idxDs]:
             lstDs[idxDs] = 0
